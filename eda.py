c9f4335e6473d8906df92b94e07ed7bb3065b1a7
Dolganova Nina <90354196+dolganovana@users.noreply.github.com>
2022-06-02 04:01:44 +0300

    Update eda.py

--- eda.py
+++ eda.py
@@ -1,17 +1,32 @@
 import pandas as pd
 df = pd.read_csv("eda.csv")
-df.sample(3)
 
 from typing import List
 from fastapi import FastAPI, Query
 app = FastAPI(
-    title="My lovely API"
+    title="Recipies Dolganova"
     )
 
 @app.get("/")
 def root()->dict:
-    return{"message": "Hello world!"}
+    return{"message": "Hello!"}
 
 @app.get("/items/")
 def read_items(q: List[int] = Query(None)):
     return {"q": q}
+
+@app.get('/recipes/diet/')
+def recipes_by_calories(calories: int, top_n: int) -> dict:
+    suitable = df[df['cal'] <= calories]
+    top_sorted = suitable.sort_values(by='cal').head(top_n)
+    logger.info(f"{top_n} most suitable recipes under {calories} calories successfully fetched")
+    return top_sorted.to_dict(orient='index')
+
+@app.get('/recipes/')
+def recipes_by_difficulty(difficulty: str, top_n: int) -> dict:
+    ascending = True if difficulty == 'simple' else False
+    new_df = pd.DataFrame().assign(id = df['id'], name = df['name'], steps = df['list_resipe'])
+    new_df['difficulty'] = new_df.apply(lambda row: len(row.steps), axis=1)
+    new_df.drop('steps', axis=1)
+    top_sorted = new_df.sort_values(by='difficulty', ascending=ascending).head(top_n)
+    return top_sorted.to_dict(orient='index')
